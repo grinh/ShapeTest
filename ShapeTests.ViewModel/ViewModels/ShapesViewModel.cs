7cecbfb3ec2792be2a073fd006871dda620650d6
Tomasz Szczepanik <Tomasz Szczepanik>
2016-06-11 23:08:46 +0200

    Add comments to ShapesViewModel

--- ShapeTests.ViewModel/ViewModels/ShapesViewModel.cs
+++ ShapeTests.ViewModel/ViewModels/ShapesViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using MvvmCross.Core.ViewModels;
 using ShapeTest.Business.Entities;
 using ShapeTest.Business.Enums;
@@ -15,26 +16,73 @@ namespace ShapeTests.ViewModel
 {
     public class ShapesViewModel : ViewModel
     {
+		/// <summary>
+		/// Reference to shapes repository
+		/// </summary>
         private readonly IShapesRepository _ShapesRepo;
+
+		/// <summary>
+		/// Reference to service that can compute total area of all shapes
+		/// </summary>
         private readonly IComputeAreaService _ComputeAreaService;
+
+		/// <summary>
+		/// Reference to a service that can submit data
+		/// </summary>
         private readonly ISubmissionService _SubmissionService;
 
-		
-
+		/// <summary>
+		/// Contains information if currently any submission is in progress
+		/// </summary>
 	    private bool _SubmissionInProgress;
+
+		/// <summary>
+		/// Constains result of last sumbmission.
+		/// </summary>
 	    private SubmissionResult _SubmissionResult;
 
+		/// <summary>
+		/// Contains list of shapes
+		/// </summary>
         private ObservableCollection<IShape> _Shapes;
 
+		/// <summary>
+		/// Contains currently selected shape
+		/// </summary>
         private IShape _SelectedShape;
 
+		/// <summary>
+		/// Computed total area 
+		/// </summary>
         private double _TotalArea;
 
-        private MvxCommand _AddTriangleCommand;
-        private MvxCommand _RemoveTriangleCommand;
-        private MvxCommand _ComputeAreaCommand;
-        private MvxCommand _SubmitAreaCommand;
+		/// <summary>
+		/// Command invoked when user wants to add shape
+		/// </summary>
+        private ICommand _AddShapeCommand;
 
+		/// <summary>
+		/// Command invoked when user wants to remove shape from shapes list
+		/// </summary>
+        private ICommand _RemoveShapeCommand;
+
+		/// <summary>
+		/// Command invoked when user wants to compute total area of all shapes
+		/// </summary>
+        private ICommand _ComputeAreaCommand;
+
+		/// <summary>
+		/// Command invoked when user wants to submit data
+		/// </summary>
+        private IMvxCommand _SubmitAreaCommand;
+
+
+		/// <summary>
+		/// Class constructor
+		/// </summary>
+		/// <param name="shapeRepo"></param>
+		/// <param name="computeAreaService"></param>
+		/// <param name="submissionService"></param>
         public ShapesViewModel(IShapesRepository shapeRepo, 
                                IComputeAreaService computeAreaService,
                                ISubmissionService submissionService)
@@ -43,60 +91,88 @@ namespace ShapeTests.ViewModel
             _ComputeAreaService = computeAreaService;
             _SubmissionService = submissionService;
 
-            AddTriangleCommand = new MvxCommand(AddTriangle);
-            RemoveTriangleCommand = new MvxCommand(RemoveSelectedTriangle);
+            AddShapeCommand = new MvxCommand(AddShape);
+            RemoveShapeCommand = new MvxCommand(RemoveSelectedShape);
             ComputeAreaCommand = new MvxCommand(ComputeTotalArea);
             SubmitAreaCommand = new MvxCommand(SubmitArea, CanSubmitArea);
         }
 
-	    private bool CanSubmitArea()
+		/// <summary>
+		/// "CanExecute" method for SubmitAreaCommand
+		/// </summary>
+		/// <returns></returns>
+		private bool CanSubmitArea()
 	    {
 		    return !_SubmissionInProgress;
 	    }
 
+		/// <summary>
+		/// Contains list of shapes
+		/// </summary>
 	    public ObservableCollection<IShape> Shapes
         {
             get { return _Shapes; }
             set { SetAndRaisePropertyChanged(ref _Shapes, value); }
         }
 
+		/// <summary>
+		/// Contains currently selected shape
+		/// </summary>
         public IShape SelectedShape
         {
             get { return _SelectedShape; }
             set { SetAndRaisePropertyChanged(ref _SelectedShape, value); }
         }
 
+		/// <summary>
+		/// Contains computed total area of all shapes
+		/// </summary>
         public double TotalArea
         {
             get { return _TotalArea; }
             set { SetAndRaisePropertyChanged(ref _TotalArea, value); }
         }
 
-        public MvxCommand AddTriangleCommand
+		/// <summary>
+		/// Command invoked when user wants to add new shape to shapes list
+		/// </summary>
+        public ICommand AddShapeCommand
         {
-            get { return _AddTriangleCommand; }
-            set { SetAndRaisePropertyChanged(ref _AddTriangleCommand, value);}
+            get { return _AddShapeCommand; }
+            set { SetAndRaisePropertyChanged(ref _AddShapeCommand, value);}
         }
 
-        public MvxCommand RemoveTriangleCommand
+		/// <summary>
+		/// Command invoked when user wants to remove shape from shapes list
+		/// </summary>
+		public ICommand RemoveShapeCommand
         {
-            get { return _RemoveTriangleCommand; }
-            set { SetAndRaisePropertyChanged(ref _RemoveTriangleCommand, value); }
+            get { return _RemoveShapeCommand; }
+            set { SetAndRaisePropertyChanged(ref _RemoveShapeCommand, value); }
         }
 
-        public MvxCommand ComputeAreaCommand
+		/// <summary>
+		/// Command invoked when user wants to compute total area of all shapes
+		/// </summary>
+		public ICommand ComputeAreaCommand
         {
             get { return _ComputeAreaCommand; }
             set { SetAndRaisePropertyChanged(ref _ComputeAreaCommand, value); }
         }
 
-        public MvxCommand SubmitAreaCommand
+		/// <summary>
+		/// Command invoked when user wants to submit data
+		/// </summary>
+		public IMvxCommand SubmitAreaCommand
         {
             get { return _SubmitAreaCommand; }
             set { SetAndRaisePropertyChanged(ref _SubmitAreaCommand, value); }
         }
 
-	    public bool SubmissionInProgress
+		/// <summary>
+		/// Contains information if currently any submission is in progress
+		/// </summary>
+		public bool SubmissionInProgress
 	    {
 		    get
 		    {
@@ -108,7 +184,10 @@ namespace ShapeTests.ViewModel
 		    }
 		}
 
-	    public SubmissionResult SubmissionResult
+		/// <summary>
+		/// Constains result of last sumbmission.
+		/// </summary>
+		public SubmissionResult SubmissionResult
 	    {
 		    get { return _SubmissionResult; }
 		    set { SetAndRaisePropertyChanged(ref _SubmissionResult, value); }
@@ -122,17 +201,29 @@ namespace ShapeTests.ViewModel
             _ShapesRepo.ShapeAdded += OnShapeAdded;
         }
 
-        public void AddTriangle()
+		/// <summary>
+		/// Handler for AddShapeCommand
+		/// </summary>
+		public void AddShape()
         {
             ShowViewModel<AddShapeViewModel>();
         }
 
+		/// <summary>
+		/// Event handler. Invoked whenever new shape is added to shape repository
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="args"></param>
         public void OnShapeAdded(object sender, ShapeEventArgs args)
         {
             Shapes.Add(args.Shape);
         }
 
-        public void RemoveSelectedTriangle()
+
+		/// <summary>
+		/// Handler for RemoveShapeCommand
+		/// </summary>
+        public void RemoveSelectedShape()
         {
             if (SelectedShape != null)
             {
@@ -142,12 +233,18 @@ namespace ShapeTests.ViewModel
             }
         }
 
-        public void ComputeTotalArea()
+		/// <summary>
+		/// Handler for ComputeAreaCommand
+		/// </summary>
+		public void ComputeTotalArea()
         {
             TotalArea = _ComputeAreaService.ComputeTotalArea();
         }
 
-        public async void SubmitArea()
+		/// <summary>
+		/// Handler for SubmitAreaCommand. It will try to submit computed area.
+		/// </summary>
+		public async void SubmitArea()
         {
 	        if (SubmissionInProgress)
 	        {
