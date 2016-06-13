50381e46a230bbb3d2227151a163d293836d9998
grinh <tom.e.k@wp.pl>
2016-06-13 22:44:35 +0200

    Add tests for ShapeApplication

---
+++ ShapeTest.ViewModel.UnitTests/ShapeApplicationTests.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvvmCross.Core.ViewModels;
+using MvvmCross.Platform;
+using ShapeTest.Business.Repositories;
+using ShapeTest.Business.Services;
+using ShapeTest.ViewModel.UnitTests.Helpers;
+using ShapeTests.ViewModel;
+
+namespace ShapeTest.ViewModel.UnitTests
+{
+    [TestClass]
+    public class ShapeApplicationTests
+    {
+        private MvvmCrossTestSetup TestSetup;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            TestSetup = new MvvmCrossTestSetup();
+            TestSetup.Setup();
+        }
+
+        [TestMethod]
+        public void ShouldRegisterInterfaces()
+        {
+            IMvxAppStart appStart;
+            IShapesRepository shapesRepository;
+            IComputeAreaService computeAreaService;
+            ISubmissionService submissionService;
+
+            var application = new ShapeApplication();
+            application.Initialize();
+
+            appStart = Mvx.Resolve<IMvxAppStart>();
+            shapesRepository = Mvx.Resolve<IShapesRepository>();
+            computeAreaService = Mvx.Resolve<IComputeAreaService>();
+            submissionService = Mvx.Resolve<ISubmissionService>();
+
+            appStart.Should().NotBeNull();
+            shapesRepository.Should().NotBeNull();
+            computeAreaService.Should().NotBeNull();
+            submissionService.Should().NotBeNull();
+        }
+    }
+}
